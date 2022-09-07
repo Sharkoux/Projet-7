ba363872f2a15b6ee06b214bfe7a376118b323ba
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2022-09-07 17:17:45 +0200

    feat: add Factory and css for card, first display OK

--- script/main.js
+++ script/main.js
@@ -2,13 +2,28 @@ import { recipes } from '../data/recipes.js';
 
 async function GetData() {
   const data = recipes;
-  const jsonStrings = data.map(item => JSON.stringify(item));
+  const jsonStrings = data.map((item) => JSON.stringify(item));
   const backToNumbers = jsonStrings.map((s) => JSON.parse(s));
+
+  return {
+    backToNumbers: [...backToNumbers],
+  };
+}
+
+async function DisplayData(backToNumbers) {
+  const recipes_zone = document.querySelector('.recipes_card');
   console.log(backToNumbers);
+  backToNumbers.forEach((element) => {
+    console.log(element);
+    const recipes_content = RecipesFactory(element);
+    const recipesDOM = recipes_content.getRecipesCardDOM();
+    recipes_zone.appendChild(recipesDOM);
+  });
 }
 
-async function DisplayData(data) {
-
+async function init() {
+  const { backToNumbers } = await GetData();
+  DisplayData(backToNumbers);
 }
 
-GetData();
+init();
