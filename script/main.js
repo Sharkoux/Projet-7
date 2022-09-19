b81dbb4ec6142926c8379a203a6073b277e8d425
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2022-09-19 14:54:47 +0200

    feat: add method TagIngredientAvailable to class

--- script/main.js
+++ script/main.js
@@ -1,13 +1,16 @@
 import { recipes } from '../data/recipes.js';
 
 const SEARCHINPUT = document.querySelector('.input_search');
+
 const RECIPESZONE = document.querySelector('.recipes_card');
 const DROPDOWNMENU = document.querySelectorAll('.dropdown-menu');
 const DROPBTNPRIMARY = document.querySelector('.btn-primary');
+const DROPBTNAPPAREILS = document.querySelector('.btn-appareils');
+const DROPBTNUSTENCILS = document.querySelector('.btn-danger');
 const DROPDOWNINGREDIENT = document.querySelector('.btn-ingrédients');
-const ALLDROPDOWN = document.querySelectorAll('.dropdown');
+const DROPBTN = document.querySelector('.drop_btn');
 const ALLARROWUP = document.querySelectorAll('.angle-up');
-const array = [];
+let array = [];
 
 /* Event */
 
@@ -25,10 +28,19 @@ document.addEventListener('keydown', (e) => {
   }
 });
 
+/* function open / close */
+
 function OpenTagIngredient() {
   DROPDOWNINGREDIENT.classList.toggle('active');
+  DROPBTN.style.setProperty('border-radius', '5px 5px 0 0');
   DROPBTNPRIMARY.innerHTML = '<input class="input form-control search-ingredients" type="text" placeholder="Rechercher un ingrédient"></input><em class="fa-solid fa-angle-up angle-up"></em>';
   DROPBTNPRIMARY.removeEventListener('click', OpenTagIngredient);
+  const SEARCHTAG = document.querySelector('.search-ingredients');
+
+  SEARCHTAG.addEventListener('keyup', () => {
+    const INPUTTAG = SEARCHTAG.value.trim();
+    TagSearch(INPUTTAG);
+  });
 }
 
 function CloseTagIngredient() {
@@ -47,38 +59,37 @@ class Recipe {
     this.description = datarecipes.description;
     this.appliance = datarecipes.appliance;
     this.ustensils = datarecipes.ustensils;
+    this.availableIngredientsTags = array;
+    this.tag = [];
   }
 
-  TAG() {
+  /* method for data Tag Ingredients */
+  TagIngredientAvailable() {
+    DROPDOWNMENU[0].innerHTML = '';
+    for (let j = 0; j < this.ingredients.length; j += 1) {
+      this.availableIngredientsTags.push(this.ingredients[j].ingredient.toLocaleLowerCase());
+    }
+    const Tag = [...new Set(this.availableIngredientsTags)];
 
+    for (let e = 0; e < Tag.length; e += 1) {
+      const Finish = TagIngredient(Tag[e]);
+      DROPDOWNMENU[0].appendChild(Finish);
+    }
+    
   }
+  
 }
 
-/* function for send data to display */
+/* function send data recipes */
 
 function DisplayData(DataRecipe) {
+  array = [];
+  DROPDOWNMENU[0].innerHTML = '';
   for (let a = 0; a < DataRecipe.length; a += 1) {
     const NEWRECIPES = new Recipe(DataRecipe[a]);
+    NEWRECIPES.TagIngredientAvailable();
     const recipesDOM = createRecipesCardDOM(NEWRECIPES);
     RECIPESZONE.appendChild(recipesDOM);
-  }
-}
-
-/* function send data for display TAG ingredients */
-function TagIngredientDisplay(Tagrecipes) {
-  DROPDOWNMENU[0].innerHTML = '';
-  for (let i = 0; i < Tagrecipes.length; i += 1) {
-    const { ingredients } = Tagrecipes[i];
-    console.log(ingredients);
-    for (let j = 0; j < ingredients.length; j += 1) {
-      array.push(ingredients[j].ingredient.toLocaleLowerCase());
-    }
-  }
-  const Tag = [...new Set(array)];
-  console.log(Tag);
-  for (let e = 0; e < Tag.length; e += 1) {
-    const Finish = TagIngredient(Tag[e]);
-    DROPDOWNMENU[0].appendChild(Finish);
   }
 }
 
@@ -86,45 +97,52 @@ function TagIngredientDisplay(Tagrecipes) {
 function Search(INPUT) {
   if (INPUT.length > 2) {
     const ARRAYFILTER = [];
-    
-    
-    
-   // algo search recette full boucle Native
+
+    // algo search recette full boucle Native
     for (let z = 0; z < recipes.length; z += 1) {
       if (recipes[z].name.toLocaleLowerCase().includes(INPUT.toLocaleLowerCase())) {
         ARRAYFILTER.push(recipes[z]);
       }
-      if (recipes[z].description.includes(INPUT)) {
+      if (recipes[z].description.includes(INPUT.toLocaleLowerCase())) {
         ARRAYFILTER.push(recipes[z]);
       }
 
       for (let x = 0; x < recipes[z].ingredients.length; x += 1) {
         const INGREDIENTS = recipes[z].ingredients;
-        console.log(INGREDIENTS);
-        if (INGREDIENTS[x].ingredient.includes(INPUT)) {
+
+        if (INGREDIENTS[x].ingredient.includes(INPUT.toLocaleLowerCase())) {
           ARRAYFILTER.push(recipes[z]);
         }
       }
     }
 
-    console.log(ARRAYFILTER);
-    const RESULTFILTER = [... new Set(ARRAYFILTER)];
-   
-   RECIPESZONE.innerHTML = '';
-    /*
-    for (let e = 0; e < RESULTFILTER.length; e += 1) {
-      console.log(RESULTFILTER[0].ingredients)
-      const Finish = TagIngredient(RESULTFILTER[e].ingredient);
-      DROPDOWNMENU[0].appendChild(Finish);
-    }
-    */
+    const RESULTFILTER = [...new Set(ARRAYFILTER)];
+    RECIPESZONE.innerHTML = '';
+
     DisplayData(RESULTFILTER);
   }
 }
 
-async function init() {
-  DisplayData(recipes);
-  TagIngredientDisplay(recipes);
+function TagSearch(INPUTTAG) {
+  const ARRAYSEARCHTAG = [];
+ 
+  if (INPUTTAG.length > 2) {
+    for (let w = 0; w < recipes.length; w += 1) {
+      for (let x = 0; x < recipes[w].ingredients.length; x += 1) {
+        const TAGS = recipes[w].ingredients;
+
+        if (TAGS[x].ingredient.toLocaleLowerCase().includes(INPUTTAG.toLocaleLowerCase())) {
+         ARRAYSEARCHTAG.push(TAGS[x]);
+        }
+      }
+    }
+  }
+  const Finish = TagIngredient(ARRAYSEARCHTAG);
+  DROPDOWNMENU[0].appendChild(Finish);
 }
 
-init();
+
+DisplayData(recipes);
+
+
+
